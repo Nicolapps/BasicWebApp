82b05af2a3cd43e469a838707d9831550c16860c
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-09-20 23:02:01 -0400

    Fix XSS

--- src/main/java/com/develogical/web/ResultsPage.java
+++ src/main/java/com/develogical/web/ResultsPage.java
@@ -17,12 +17,28 @@ public class ResultsPage extends HtmlPage {
 
         if (answer == null || answer.isEmpty()) {
             writer.println("<h1>Sorry</h1>");
-            writer.print("<p>Sorry, we didn't understand <em>" + query + "</em></p>");
+            writer.print("<p>Sorry, we didn't understand <em>" + escapeHTML(query) + "</em></p>");
         } else {
-            writer.println("<h1>" + query + "</h1>");
-            writer.println("<p>" + answer + "</p>");
+            writer.println("<h1>" + escapeHTML(query) + "</h1>");
+            writer.println("<p>" + escapeHTML(answer) + "</p>");
         }
 
         writer.println("<p><a href=\"/\">Back to Search Page</a></p>");
+    }
+
+    // https://stackoverflow.com/a/25228492/4652564
+    public static String escapeHTML(String s) {
+        StringBuilder out = new StringBuilder(Math.max(16, s.length()));
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c > 127 || c == '"' || c == '\'' || c == '<' || c == '>' || c == '&') {
+                out.append("&#");
+                out.append((int) c);
+                out.append(';');
+            } else {
+                out.append(c);
+            }
+        }
+        return out.toString();
     }
 }
