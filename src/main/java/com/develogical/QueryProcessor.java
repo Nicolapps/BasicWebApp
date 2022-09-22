cdc139a731628af30a6214ce60eba22e7c02c45f
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-09-22 15:39:38 -0400

    prog

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -18,17 +18,27 @@ public class QueryProcessor {
             return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
         }
 
-        String pattern = "what is (\\d+) plus (\\d+)";
-        Matcher m = Pattern.compile(pattern).matcher(query);
-        if (m.find()) {
-            String s1 = m.group(1);
-            String s2 = m.group(2);
+        Matcher m1 = Pattern.compile("what is (\\d+) plus (\\d+)").matcher(query);
+        if (m1.find()) {
+            String s1 = m1.group(1);
+            String s2 = m1.group(2);
             int res = (Integer.parseInt(s1) + Integer.parseInt(s2));
             return Integer.toString(res);
         }
-        String number = m.group(1);
-        System.out.println(number);
-        System.out.println("Here are your " + number + " apples");
+
+        Matcher m2 = Pattern.compile("which of the following numbers is the largest: (.+)").matcher(query);
+        if (m2.find()) {
+            String numbers = m2.group(1);
+            String[] numbersStr = numbers.split(", ");
+
+            int largest = Integer.MIN_VALUE;
+            for (String x : numbersStr) {
+
+            }
+
+//            return Integer.toString(res);
+        }
+
 
         return "";
     }
