1fe081858cde8ed1fb10108ad31648a632c1e20a
Nicolas Ettlin <nicolas.ettlin@me.com>
2022-09-22 15:19:34 -0400

    Q1

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -14,6 +14,9 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("lorem")) {
             return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
         }
+        if (query.toLowerCase().contains("what is your name")) {
+            return "My name is Bobot.";
+        }
         return "";
     }
 }
